e1ff5307def8158697a9aadfcced06e35b621b5e
Sho Yamane <sns.yamane@gmail.com>
2017-10-14 09:43:57 +0900

    screenshot

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php get_header(); ?>
 
-<img src="<?php echo get_template_directory_uri(); ?>/dist/img/sample.jpg">
-<div class="text">index.php</div>
+<img src="<?php echo get_template_directory_uri(); ?>/dist/img/sample.jpg"><div class="text">index.php</div>
 
 <?php get_footer(); ?>
